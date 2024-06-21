06d277764b0bca02cb95929d9cb7db3ba7c3c0db
boris <boristestud@gmail.com>
2024-06-21 10:12:54 +0200

    commented code

--- arduino/main/src/Leds.cpp
+++ arduino/main/src/Leds.cpp
@@ -13,8 +13,11 @@ void Leds::setup()
 
 void Leds::blink(char* color, int interval, unsigned long time)
 {
+    // Gets the `uint32_t` color value based on the color provided as a parameter. 
     uint32_t colorValue = getColor(color);
 
+    // Checking if the the time provided in the `interval` parameter has passed.
+    // This avoids using delays and thus blocking the code.
     if (time - previousLedMillis >= interval)
     {
         previousLedMillis = time;
@@ -34,8 +37,9 @@ void Leds::blink(char* color, int interval, unsigned long time)
     }
 }
 
-void Leds:: still(char* color)
+void Leds::still(char* color)
 {
+    // Gets the `uint32_t` color value based on the color provided as a parameter.
     uint32_t colorValue = getColor(color);
 
     ledStrip.fill(colorValue);
@@ -45,14 +49,18 @@ void Leds:: still(char* color)
 
 void Leds::forwardBlink(char* color, int interval, unsigned long time)
 {
+    // Gets the `uint32_t` color value based on the color provided as a parameter.
     uint32_t colorValue = getColor(color);
 
+    // Checking if the the time provided in the `interval` parameter has passed.
+    // This avoids using delays and thus blocking the code.
     if (time - previousLedMillis >= interval)
     {
         previousLedMillis = time;
 
         if (ledIsOn == 0)
         {
+            // Only lights up 17 LEDs starting from the 3rd one.
             ledStrip.fill(colorValue, 3, 17);
             ledStrip.show();
         }
@@ -66,14 +74,18 @@ void Leds::forwardBlink(char* color, int interval, unsigned long time)
 
 void Leds::backwardBlink(char* color, int interval, unsigned long time)
 {
+    // Gets the `uint32_t` color value based on the color provided as a parameter.
     uint32_t colorValue = getColor(color);
 
+    // Checking if the the time provided in the `interval` parameter has passed.
+    // This avoids using delays and thus blocking the code.
     if (time - previousLedMillis >= interval)
     {
         previousLedMillis = time;
 
         if (ledIsOn == 0)
         {
+            // Only lights up 13 LEDs starting from the 20th.
             ledStrip.fill(colorValue, 20, 13);
             // Needed because we reach the end of the strip with the line above so we need
             // to start over from 0 to complete the half circle.
@@ -90,19 +102,28 @@ void Leds::backwardBlink(char* color, int interval, unsigned long time)
 
 void Leds::initializing(int interval, unsigned long time)
 {
+    // Gets the `uint32_t` color value based on the color provided as a parameter.
     uint32_t colorValue = getColor("magenta");
 
+    // Checking if the the time provided in the `interval` parameter has passed.
+    // This avoids using delays and thus blocking the code.
     if (time - previousLedMillis >= interval)
     {
         previousLedMillis = time;
 
         if (ledIsOn == 0)
         {
+            // Lights up LEDs incrementally 
             for (int i = 0; i < ledStrip.numPixels() + 1; i++)
             {
                 ledStrip.fill(colorValue, 0, i);
                 ledStrip.show();
                 ledStrip.clear();
+                // TEST: 
+                // ledStrip.fill(colorValue, i, 1);
+                // ledStrip.show();
+
+                // TODO: light up speed depends on interval
             }
         }
     }
--- arduino/main/src/Leds.h
+++ arduino/main/src/Leds.h
@@ -6,7 +6,6 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 
-
 class Leds
 {
     public:
@@ -30,17 +29,17 @@ class Leds
         void still(char* color);
 
         /**
-         * Blinks only the forward half of the strip
+         * Blinks only the forward half of the strip.
          */
         void forwardBlink(char* color, int interval, unsigned long time);
 
         /**
-         * Blinks only the backward half of the strip
+         * Blinks only the backward half of the strip.
          */
         void backwardBlink(char* color, int interval, unsigned long time);
 
         /**
-         * TODO
+         * Displays the battery voltage status of the robot (NOT IMPLEMENTED YET).
          */
         void batteryVoltage();
         
@@ -50,8 +49,9 @@ class Leds
         void initializing(int interval, unsigned long time);
 
         /**
-         * Given the color name, returns it's `uint32_t` representation.
-         * Call this method to access the read only color definitions.
+         * Given the color name as a string, returns it's `uint32_t` representation.
+         * This method is used so that a simple string can be provided as a parameter
+         * 
          */
         inline uint32_t getColor(const char* color) 
         {
@@ -65,30 +65,43 @@ class Leds
     private:
 
         /** 
-         * Params to control the LEDs.
+         * Params to control the LED strip.
          */
         const int ledPin=2, numLeds=33, brightness=50;
 
+        /**
+         * State of the LED strip used for the blinking effect.
+         */
         int ledIsOn = 0;
+
+        /**
+         * Keeps track of the last 
+         */
         unsigned long previousLedMillis = 0;
 
+        /**
+         * ledStrip object initialization.
+         */
         Adafruit_NeoPixel ledStrip = Adafruit_NeoPixel(numLeds, ledPin, NEO_GRB + NEO_KHZ800);
 
+        /**
+         * Structure for the Colors data type which associates a color name with its `uint32_t` representation.
+         */
         struct Colors {
             const char* color;
             uint32_t colorValue;
         };
 
         /**
-         * Key value pair of all the colors associated with their `uint32_t` representation.
+         * Key value pairs of all the colors you can use associated with their `uint32_t` representation.
+         * Feel free to add additional colors based on your needs.
          */
         Colors colors[4] = {
             {"red", ledStrip.Color(255, 0, 0)},
             {"green", ledStrip.Color(0, 255, 0)},
             {"blue", ledStrip.Color(0, 0, 255)},
-            {"magenta", ledStrip.Color(255, 0, 255)},
+            {"magenta", ledStrip.Color(255, 0, 255)}
         };
-
 };
 
 #endif
