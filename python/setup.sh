cf5121185ce232ecdcc1e19b10b164bcc383e26e
MarsiaSec <boristestud@gmail.com>
2024-06-24 18:00:20 +0200

    setup script

--- python/setup.sh
+++ python/setup.sh
@@ -1,31 +1,28 @@
 #! /bin/bash
 
+# Updates the OS
 sudo apt update && sudo apt -y upgrade
 
-#sudo apt install -y subversion
-
+# Creates the directory where the repo will get cloned
 sudo mkdir /opt/starflit
 
+# Changes ownership of the starflit directory
 sudo chown ${USER} /opt/starflit 
 
-cd /opt/starflit
+# Clones the starflit repository
+git clone https://github.com/lbo462/starflit.git /opt/starflit
 
-pwd
+# Creates a virtual env
+virtualenv --system-site-packages /opt/starflit/starflit/python/venv
 
-#svn export https://github.com/lbo462/starflit/trunk/python 
+# Activates the virtual env
+source /opt/starflit/starflit/python/venv/bin/activate
 
-git clone https://github.com/lbo462/starflit.git
+# Installs the requirements
+pip install -r /opt/starflit/starflit/python/requirements.txt
 
-cd starflit
+# Creates the .env file based on the default.env file provided in the repository
+mv /opt/starflit/starflit/python/default.env /opt/starflit/starflit.python/.env
 
-cd python
-
-virtualenv --system-site-packages venv
-
-source venv/bin/activate
-
-pip install -r requirements.txt
-
-mv default.env .env
-
-python3 main.py --model mobilenet_v2.tflite --label labels.txt --obj "stop sign"
+# Deactivates the virtual env
+deactivate
