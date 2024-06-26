e6b4211acdea5a960385a49838098157f485298e
Léo B. <leo.bonnaire@proton.me>
2024-06-26 12:29:40 +0200

    WIP changed folder name

---
+++ raspberry/install.sh
@@ -0,0 +1,38 @@
+#! /bin/bash
+#
+#       ~ INSTALL SCRIPT FOR RPi ~
+#
+#   This is to be executed on a freshly flashed RPi.
+#   It installs all the necessary component to control
+#   the strandbeest to do some object recognition.
+#
+#   It also install the systemD so that the python
+#   script that exhanges data with the Arduino gets
+#   executed during the RPi boot.
+#
+
+export GIT_REPO_URL="https://github.com/lbo462/starflit.git"
+
+# Creates the directory where the repo will get cloned
+sudo mkdir /opt/starflit && sudo chown ${USER} /opt/starflit 
+
+# Clones the starflit repository in the /opt/starflit
+git clone ${GIT_REPO_URL} /opt/starflit/
+
+# Creates a virtual env for executing python code
+virtualenv --system-site-packages /opt/starflit/venv
+
+# Installs the requirements in the venv
+/opt/starflit/venv/bin/pip install -r /opt/starflit/raspberry/python/requirements.txt
+
+# Creates the .env file based on the default.env file provided in the repository
+cp /opt/starflit/raspberry/python/default.env /opt/starflit/raspberry/python/.env
+
+# Copy the service where it should land ...
+cp /opt/starflit/raspberry/starflit.service /etc/systemd/system/
+
+# ... and enable it to run on boot.
+sudo systemctl enable starflit.service
+
+# And then start the service so that is runs directly
+sudo systemctl start starflit.service
